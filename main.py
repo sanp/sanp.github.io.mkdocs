403b2f98407f281cf1e83447036c4bec9a3ac88f
Steve SanPietro <steven.sanpietro@rate.com>
2019-12-27 16:59:48 -0600

    Test

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
 def define_env(env):
   pass
+
+def declare_variables(variables, macro):
+    """
+    This is the hook for the functions
+
+    - variables: the dictionary that contains the variables
+    - macro: a decorator function, to declare a macro.
+    """
+
+    variables['baz'] = "John Doe"
+
+    @macro
+    def bar(x):
+        return (2.3 * x) + 7
+
+
+
+    # If you wish, you can  declare a macro with a different name:
+    def f(x):
+        return x * x
+
+    macro(f, 'barbaz')
+
+    # or to export some predefined function
+    import math
+    macro(math.floor) # will be exported as 'floor'
